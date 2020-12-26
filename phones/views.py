4293d2d94fdd528680c8cf4ddc22a97165f07a22
Андрей Хвостов <HomeMacbook@MacBook-Pro-Andrej.local>
2020-12-26 10:55:02 +0300

    hw v1.0

--- phones/views.py
+++ phones/views.py
@@ -3,13 +3,29 @@ from phones.models import Phone
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort', 'id')
+    sort_sources = {
+        'name': 'name',
+        'lowprice': 'price',
+        'maxprice': '-price',
+        'id': 'id',
+    }
+    if sort not in sort_sources.keys():
+        sort = 'id'
+
     template = 'catalog.html'
-    phones = Phone.objects.all()
-    context = {'phones': phones}
+    phones = Phone.objects.order_by(sort_sources[sort]).all()
+    context = {'phones': phones,
+               'sort': sort,
+               }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug).get()
+    context = {
+        'phone': phone,
+    }
+    print(f'phone = {phone}')
     return render(request, template, context)
